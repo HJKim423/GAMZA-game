43afa14605ec0e67bfd91ec98ac72d22be6beaef
HJ Kim <pknojlh27@naver.com>
2022-04-04 18:53:17 +0900

    찐최종 수정

--- main.js
+++ main.js
@@ -80,13 +80,13 @@ function moveCactus(){
         if(a.x < 0){
             o.splice(i,1);
         }
-        if(timer < 200) a.x -= 11;
-        else if(timer < 400) a.x -= 13;
-        else if(timer <600) a.x -= 15;
-        else if(timer < 800) a.x -= 17;
-        else if(timer <900) a.x -= 19;
-        else if(timer <1200) a.x -= 21;
-        else if(timer <1500) a.x -= 23;
+        if(timer < 200) a.x -= 12;
+        else if(timer < 400) a.x -= 14;
+        else if(timer <600) a.x -= 16;
+        else if(timer < 800) a.x -= 18;
+        else if(timer <900) a.x -= 20;
+        else if(timer <1200) a.x -= 23;
+        else if(timer <1500) a.x -= 24;
         else if(timer <3300) a.x -= 26;
         else if(timer <4000) a.x -= 30;
 
